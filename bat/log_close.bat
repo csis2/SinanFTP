ebd34eddd5a04308b666bc0fb663756ae903cca9
csisgo <39471525+csisgo@users.noreply.github.com>
2018-06-25 22:21:58 -0300

    Delete log_close.bat

--- bat/log_close.bat
+++
@@ -1,6 +0,0 @@
-echo %time:~0,2%:%time:~3,2%:%time:~6,2% %date:~-10,2%/%date:~-7,2%/%date:~-4,4%-------Renomeando e finalizando arquivo de log. >>c:\sinanftp\log\sinanFTP.log
-echo Renomeando log de eventos
-cd\
-cd c:\sinanftp\log
-rename SinanFTP.log SinanFTP_%date:~-10,2%%date:~-7,2%%date:~-4,4%_%time:~0,2%%time:~3,2%%time:~6,2%.log
-del /F /Q SinanFTP.log
